da54756dd928a7e3115e48051ee7e0c43e78e7ac
tysongouche <39385160+tysongouche@users.noreply.github.com>
2023-12-12 20:44:19 -0500

    Update testing_student.py

--- testing_student.py
+++ testing_student.py
@@ -64,6 +64,16 @@ class TestStudent(unittest.TestCase):
 
         self.assertEqual(output, expected_output)
 
+    def chatbot_class(self):
+        #import openai
+        from dotenv import load_dotenv, find_dotenv   # get the system environment variables
+        import sys  # get command line arguments        
+        from langchain.llms import OpenAI        
+        load_dotenv(find_dotenv())  # load the environment variables      
+        query = sys.argv[1] # get the argument string command line   
+        llm = OpenAI(model_name="text-davinci-003") #instantiate a Davinci LLM        
+        result = llm(query) # execute a query from command line
+        print("Query #1 (CMDLINE) *****\n" + result, end='\n')
 
 if __name__ == "__main__":
     unittest.main()
